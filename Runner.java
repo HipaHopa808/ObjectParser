50ab9792a380da8f470e311bab6abd5981f6009a
Hipahopa <dmitry.sseu@yandex.ru>
2026-01-30 12:22:13 +0400

    Убрана зависимость Jakson, отключен Maven

--- Runner.java
+++ Runner.java
@@ -1,9 +1,8 @@
-import com.fasterxml.jackson.databind.ObjectMapper;
+
 
 public class Runner {
     public static void main(String[] args) {
-        ObjectMapper mapper = new ObjectMapper();
-        System.out.println(mapper);
+
 
     }
 }
